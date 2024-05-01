3ac8bc08678fd6d265c06d19e90fee41f025a8e5
liao <2018003549@qq.com>
2024-05-01 21:27:11 +0800

    最后只是测试了一下UV统计功能，并没有具体实现

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -140,7 +140,24 @@ class HmDianPingApplicationTests {
                         new Point(shop.getX(), shop.getY())
                 ));
             }
-            stringRedisTemplate.opsForGeo().add(key,locations);
+            stringRedisTemplate.opsForGeo().add(key, locations);
         }
+    }
+
+    @Test
+    void testHyperLogLog() {
+        // 准备数组，装用户数据
+        String[] users = new String[1000];
+        int index = 0;
+        for (int i = 1; i <= 1000000; i++) {
+            users[index++] = "user_" + i;
+            // 每1000条发送一次
+            if (i % 1000 == 0) {
+                index = 0;
+                stringRedisTemplate.opsForHyperLogLog().add("hll1", users);
+            }
+        }    // 统计数量
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("hll1");
+        System.out.println("size = " + size);
     }
 }
